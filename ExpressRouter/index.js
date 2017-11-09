60d1dd8fbdb7698b01f744a6c2a4662c4f226d5c
wh <wang1386528@gmail.com>
2017-11-09 18:19:50 +0800

    nodejs myblog

--- ExpressRouter/index.js
+++ ExpressRouter/index.js
@@ -1,8 +1,13 @@
+var path = require("path");
 var express = require("express");
 var app = express();
 
 var indexRouter = require("./router/index");
 var usersRouter = require("./router/users");
+//设定模板引擎
+app.set('views', path.join(__dirname, 'views'));// 设置存放模板文件的目录
+app.set('view engine', 'ejs');// 设置模板引擎为 ejs
+
 
 app.use("/",indexRouter);
 app.use("/users",usersRouter);
