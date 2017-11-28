c55bed467173b702bf69b44cc89033dbedf3635f
wang33 <wang1386528@gmail.com>
2017-11-28 17:55:03 +0800

    nginx test

--- MyBlog/routes/users.js
+++ MyBlog/routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/',function (req, res, next) {
+    res.send("hello darkness my old friend");
+})
+
 /* GET users listing. */
 router.get('/:name', function(req, res, next) {
   res.render("index",{title : req.params.name});
