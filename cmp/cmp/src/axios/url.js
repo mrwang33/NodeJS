e01dac07cb9e1b01a3763464f967068e5661d3e3
wh <wang1386528@gmail.com>
2018-06-15 09:31:00 +0800

    delete other code

--- cmp/cmp/src/axios/url.js
+++
@@ -1,6 +0,0 @@
-export default {
-    // 接口代理配置
-    urlWeb: 'http://www.zjwocloud.cn/cucmp-web',
-    urlUser: 'http://www.zjwocloud.cn/cucmp-user',
-    urlEcs: 'http://www.zjwocloud.cn/cucmp-ecs'
-}
